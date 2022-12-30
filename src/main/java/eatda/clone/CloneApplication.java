f9ee07e775cf35f1d2023bfa429648239666d037
SeoJiHyeon <sgh9702@naver.com>
2022-12-30 16:13:02 +0900

    초기 dev 세팅

---
+++ src/main/java/eatda/clone/CloneApplication.java
@@ -0,0 +1,12 @@
+package eatda.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneApplication.class, args);
+	}
+}
---
+++ src/test/java/eatda/clone/CloneApplicationTests.java
@@ -0,0 +1,13 @@
+package eatda.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
