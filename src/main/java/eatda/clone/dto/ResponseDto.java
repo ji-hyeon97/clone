4a3fb77c0f8d8483eddfa3d199015736aef96a93
SeoJiHyeon <sgh9702@naver.com>
2023-01-05 09:21:24 +0900

    frontend 로 보내는 응답 dto 통일

---
+++ src/main/java/eatda/clone/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package eatda.clone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto<T> {
+    private int status;
+    private T data;
+}
